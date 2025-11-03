87bf1ffea0cde4249f2a49de68e7778cd8676ac2
meet5301 <meetgol22@gmail.com>
2025-11-03 15:21:21 +0530

    moved HTML files inside templates folder

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template, request, redirect,session
+# ---------- Flask + MongoDB Atlas + Authentication + Image Upload ----------
+
+from flask import Flask, render_template, request, redirect, session
 from pymongo import MongoClient
-import base64
+import base64, os
 
 app = Flask(__name__)
 app.secret_key = "secretkey"
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["crudDB"]
-col = db["users"]
-users = db['okkk']
+# ---------- MongoDB Atlas Connection ----------
+MONGO_URI = "mongodb+srv://meet:meet123@cluster0.fldq0wp.mongodb.net/?appName=Cluster01"
+client = MongoClient(MONGO_URI)
+db = client["crudDB"]      # Database name
+col = db["users"]          # For user data + photo
+users = db["okkk"]         # For login/register users
 
+# ---------- HOME PAGE ----------
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -20,10 +25,10 @@ def index():
         col.insert_one({"name": name, "age": age, "photo": photo_data})
         return redirect('/')
 
-    users = list(col.find({}, {"_id": 0}))
-    return render_template('index.html', users=users, current_user=session.get('user', 'Guest'))
+    all_users = list(col.find({}, {"_id": 0}))
+    return render_template('index.html', users=all_users, current_user=session.get('user', 'Guest'))
 
-
+# ---------- UPDATE ----------
 @app.route('/update', methods=['GET', 'POST'])
 def update():
     if request.method == 'POST':
@@ -33,6 +38,7 @@ def update():
         return redirect('/')
     return render_template('update.html')
 
+# ---------- DELETE ----------
 @app.route('/delete', methods=['GET', 'POST'])
 def delete():
     if request.method == 'POST':
@@ -40,39 +46,45 @@ def delete():
         col.delete_one({"name": name})
         return redirect('/')
     return render_template('delete.html')
-@app.route('/register',methods=['GET','POST'])
+
+# ---------- REGISTER ----------
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         uname = request.form['username']
         pwd = request.form['password']
 
-        if users.find_one({"username":uname}):
-            return "user already"
+        if users.find_one({"username": uname}):
+            return "User already exists!"
         
-        users.insert_one({"usersname":uname,"password":pwd})
+        users.insert_one({"username": uname, "password": pwd})
         return redirect('/login')
     
     return render_template('register.html')
 
-
-@app.route('/login',methods=['GET','POST'])
+# ---------- LOGIN ----------
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        uname =request.form['username']
+        uname = request.form['username']
         pwd = request.form['password']
 
-        user = users.find_one({"usersname":uname,"password":pwd})
+        user = users.find_one({"username": uname, "password": pwd})
         if user:
             session['user'] = uname
             return redirect('/')
         else:
-            return "invalid credentaials"
+            return "Invalid credentials!"
         
     return render_template('login.html')
 
-import os
+# ---------- LOGOUT ----------
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/login')
 
+# ---------- Run App ----------
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
-
